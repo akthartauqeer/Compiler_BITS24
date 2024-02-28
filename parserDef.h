3160f8b89aba0cc3ed0ce284865aaa8e49b1b84c
Dhruv Kamra <95697310+Dhruvi-2003@users.noreply.github.com>
2024-02-28 22:27:50 +0530

    Add files via upload

---
+++ parserDef.h
@@ -0,0 +1,105 @@
+#ifndef parserDef
+#define parserDef
+#include"lexerdef.h"
+
+#endif
+
+#define NTC 78
+#define TC 50
+typedef enum{
+    program, mainFunction, otherFunctions, function, input_par, output_par, parameter_list, dataType, 
+    primitiveDatatype, constructedDatatype, A, stmts, remaining_list, typeDefinitions, typeDefinition, fieldDefinitions, 
+    fieldDefinition, dataType1, moreFields, declarations, declaration, colon_or_not, global_or_not, otherStmts, stmt, 
+    assignmentStmt, singleOrRecId, singleOrRecId1, singleOrRecId2, funCallStmt, outputParameters, inputParameters, 
+    iterativeStmt, conditionalStmt, conditionalStmt1, ioStmt, arithmeticExpression, operator_arithmetic1, arithmeticExpression1,
+    operator_arithmetic2, arithmeticExpression2, operator1, operator2, booleanExpression, not_expression, not_expression2, var, 
+    logicalOp, relationalOp, returnStmt, optionalReturn, idList, more_ids, definetypestmt
+}non_terminals;
+
+typedef enum{
+    TK_GT, TK_LT, TK_ASSIGNOP, TK_COMMENT, 
+    TK_SQR, TK_SQL, TK_OR, TK_NOT, TK_AND, TK_ID, TK_FIELDID, TK_DIV, TK_MUL, TK_MINUS, TK_FUNID, TK_DOT, TK_CL, TK_OP, TK_COLON, 
+    TK_SEM, TK_RUID, TK_COMMA, TK_NUM, TK_RNUM, TK_MAIN, TK_AS, TK_CALL, TK_DEFINETYPE, TK_ELSE, TK_END, TK_ENDIF, TK_ENDWHILE, 
+    TK_ENDRECORD, TK_ENDUNION, TK_GLOBAL, TK_IF, TK_INPUT, TK_OUTPUT, TK_INT, TK_REAL, TK_LIST, TK_PARAMETERS, TK_PARAMETER, 
+    TK_READ, TK_WRITE, TK_RECORD, TK_UNION, TK_RETURN, TK_THEN, TK_TYPE, TK_WHILE, TK_WITH
+}terminals;
+
+//First follow sets
+typedef struct terminal_node {
+    terminals t;
+    struct terminal_node* next;
+} terminal_node;
+
+typedef struct terminal_list {
+    terminal_node* head;
+} terminal_list;
+
+typedef struct non_terminal_sets {
+    terminal_list* first_set;
+    terminal_list* follow_set;
+} non_terminal_sets;
+
+//Nodes of Grammar Non-Terminals
+
+typedef struct rhs_node {
+    bool isT;  // Indicates whether it's a terminal or non-terminal
+    union {
+        terminals t;            // Terminal value
+        non_terminals non_t;     // Non-terminal value
+    } value;
+    struct rhs_node* next;  // Pointer to the next node in the list
+    tree_node* ptr;
+} RHSNode;
+
+// A linked list containing a production rule
+typedef struct production_rule {
+    RHSNode* head;          // Pointer to the head of the list
+    struct production_rule* next_rule;  // Pointer to the next production rule
+} ProductionRule;
+
+// Node for RHS of production rule
+typedef struct lhs_node {
+    ProductionRule* rules;  // Pointer to linked list of production rules
+    non_terminals lhs;       // LHS non-terminal value
+} LHSNode;
+
+typedef struct grammar {
+    LHSNode* rules[NTC];
+} Grammar; 
+
+typedef struct parse_table {
+    ProductionRule* table[NTC][TC];
+} ParseTable;
+
+//parse tree
+typedef struct tree_node {
+    treeNode* parent;  
+    treeNode* firstChild;   
+    treeNode* rightSibling;   
+    token* token_ptr;          // Pointer to the token associated with the node
+    bool isT;                  
+    union {
+        terminals t;           // Terminal value
+        non_terminals non_t;    // Non-terminal value
+    } val;
+} treeNode;
+
+//stack for maintaining non-terminals
+typedef struct stack {
+    LHSNode* top;
+    int size;
+} helperStack;
+
+//follow for non-terminals initialized
+typedef struct follow {
+    RHSNode *occurrence;  
+    non_terminals parent_nt;
+    struct follow *next;  // Pointer to the next follow struct
+} followDS;
+
+followDS *follow_occurrence[NTC]; 
+Grammar *G;
+non_terminal_sets first_follow_sets[NTC]; 
+ParseTable* PT;
+helperStack* Stack;
+treeNode* root; 
