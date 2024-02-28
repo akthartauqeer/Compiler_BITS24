8752c24eaa4a27194858c35eeaa894be903eca5d
Dhruv Kamra <95697310+Dhruvi-2003@users.noreply.github.com>
2024-02-28 22:43:38 +0530

    Update parserDef.h

--- parserDef.h
+++ parserDef.h
@@ -1,28 +1,8 @@
 #ifndef parserDef
 #define parserDef
-#include"lexerdef.h"
+#include"lexerDef.h"
 
 #endif
-
-#define NTC 78
-#define TC 50
-typedef enum{
-    program, mainFunction, otherFunctions, function, input_par, output_par, parameter_list, dataType, 
-    primitiveDatatype, constructedDatatype, A, stmts, remaining_list, typeDefinitions, typeDefinition, fieldDefinitions, 
-    fieldDefinition, dataType1, moreFields, declarations, declaration, colon_or_not, global_or_not, otherStmts, stmt, 
-    assignmentStmt, singleOrRecId, singleOrRecId1, singleOrRecId2, funCallStmt, outputParameters, inputParameters, 
-    iterativeStmt, conditionalStmt, conditionalStmt1, ioStmt, arithmeticExpression, operator_arithmetic1, arithmeticExpression1,
-    operator_arithmetic2, arithmeticExpression2, operator1, operator2, booleanExpression, not_expression, not_expression2, var, 
-    logicalOp, relationalOp, returnStmt, optionalReturn, idList, more_ids, definetypestmt
-}non_terminals;
-
-typedef enum{
-    TK_GT, TK_LT, TK_ASSIGNOP, TK_COMMENT, 
-    TK_SQR, TK_SQL, TK_OR, TK_NOT, TK_AND, TK_ID, TK_FIELDID, TK_DIV, TK_MUL, TK_MINUS, TK_FUNID, TK_DOT, TK_CL, TK_OP, TK_COLON, 
-    TK_SEM, TK_RUID, TK_COMMA, TK_NUM, TK_RNUM, TK_MAIN, TK_AS, TK_CALL, TK_DEFINETYPE, TK_ELSE, TK_END, TK_ENDIF, TK_ENDWHILE, 
-    TK_ENDRECORD, TK_ENDUNION, TK_GLOBAL, TK_IF, TK_INPUT, TK_OUTPUT, TK_INT, TK_REAL, TK_LIST, TK_PARAMETERS, TK_PARAMETER, 
-    TK_READ, TK_WRITE, TK_RECORD, TK_UNION, TK_RETURN, TK_THEN, TK_TYPE, TK_WHILE, TK_WITH
-}terminals;
 
 //First follow sets
 typedef struct terminal_node {
