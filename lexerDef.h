26c84f65be8f21c1477eb554a3357bc06900092d
Dhruv Kamra <95697310+Dhruvi-2003@users.noreply.github.com>
2024-03-03 16:12:53 +0530

    Update lexerDef.h

--- lexerDef.h
+++ lexerDef.h
@@ -5,8 +5,9 @@
 #include <stdlib.h>
 #include <time.h>
 #define tablesize 10000000
-#define NTC 78
-#define TC 50
+#define NTC 54
+#define TC 58
+#define KC 28
 
 typedef enum{
     program, mainFunction, otherFunctions, function, input_par, output_par, parameter_list, dataType, 
@@ -19,28 +20,22 @@ typedef enum{
 }non_terminals;
 
 typedef enum{
-    TK_GT, TK_LT, TK_ASSIGNOP, TK_COMMENT, 
+    TK_GT, TK_LT, TK_ASSIGNOP, TK_COMMENT, TK_EQ, TK_PLUS, TK_NE, TK_LE, TK_GE, 
     TK_SQR, TK_SQL, TK_OR, TK_NOT, TK_AND, TK_ID, TK_FIELDID, TK_DIV, TK_MUL, TK_MINUS, TK_FUNID, TK_DOT, TK_CL, TK_OP, TK_COLON, 
     TK_SEM, TK_RUID, TK_COMMA, TK_NUM, TK_RNUM, TK_MAIN, TK_AS, TK_CALL, TK_DEFINETYPE, TK_ELSE, TK_END, TK_ENDIF, TK_ENDWHILE, 
     TK_ENDRECORD, TK_ENDUNION, TK_GLOBAL, TK_IF, TK_INPUT, TK_OUTPUT, TK_INT, TK_REAL, TK_LIST, TK_PARAMETERS, TK_PARAMETER, 
     TK_READ, TK_WRITE, TK_RECORD, TK_UNION, TK_RETURN, TK_THEN, TK_TYPE, TK_WHILE, TK_WITH
 }terminals;
 
-// Union for grammarItem value
-typedef union {
-    terminals t;
-    non_terminals non_t;
-} grammarItemValue;
-
-// Struct for grammarItem
-typedef struct {
-    grammarItemValue value;
-} grammarItem;
-
+//Item of a symbol table
 typedef struct sitem {
     char *lexeme;
-    grammarItem token;
+    terminals token;
     char *type;
+    int iVal;
+    int lineCount;
+    int eof;
+    float fVal;
     int scpst;
     int scpend;
     char **parameter;
@@ -50,92 +45,30 @@ typedef struct sitem {
 typedef struct {
     SymbolItem *data[tablesize];
     int currentsize;
-} SymbolTable;
+} Symboltable;
 
-SymbolTable *table;
+#define BUFFER_SIZE (1<<20)
 
-// Function prototypes
-SymbolTable *initializeSymbolTable();
-int hash(char *lexeme);
-void insert(char *lexeme, grammarItem token);
-bool lookup(char *lexeme);
+//Twin Buffering
+typedef struct {
+	char firstBuf[BUFFER_SIZE];
+	char secondBuf[BUFFER_SIZE];
+} twinBuff;
 
-// Function to initialize SymbolTable
-SymbolTable *initializeSymbolTable() {
-    SymbolTable *table = (SymbolTable *)malloc(sizeof(SymbolTable));
-    if (table == NULL) {
-        fprintf(stderr, "Memory allocation failed for SymbolTable.\n");
-        exit(EXIT_FAILURE);
-    }
-    for (int i = 0; i < tablesize; i++) {
-        table->data[i] = NULL;
-    }
-    table->currentsize = 0;
-    return table;
-}
+typedef struct keyword{
+    char* key;
+    terminals token;
+}keyword;
 
-// Function to calculate hash
-int hash(char *lexeme) {
-    unsigned long hash = 5381;
-    int c;
-    while ((c = *lexeme++)) {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return hash % tablesize;
-}
+keyword* keywords[KC];
 
-// Function to insert into SymbolTable
-void insert(char *lexeme, grammarItem token) {
-    int index = hash(lexeme);
+Symboltable * table;
 
-    // Check if the lexeme already exists
-    if (!lookup(lexeme)) {
-        // Lexeme doesn't exist, create a new SymbolItem
-        SymbolItem *newItem = (SymbolItem *)malloc(sizeof(SymbolItem));
-        if (newItem == NULL) {
-            fprintf(stderr, "Memory allocation failed for SymbolItem.\n");
-            exit(EXIT_FAILURE);
-        }
-        newItem->lexeme = strdup(lexeme); // Make a copy of lexeme
-        if (newItem->lexeme == NULL) {
-            fprintf(stderr, "Memory allocation failed for lexeme.\n");
-            exit(EXIT_FAILURE);
-        }
-        newItem->token = token;
-        newItem->next = NULL; // Initialize next pointer
-
-        // Insert newItem into the symbol table
-        if (table->data[index] == NULL) {
-            // No collision, insert newItem directly
-            table->data[index] = newItem;
-        } else {
-            // Collision occurred, append newItem to the existing linked list
-            SymbolItem *current = table->data[index];
-            while (current->next != NULL) {
-                current = current->next;
-            }
-            current->next = newItem;
-        }
-        table->currentsize++;
-    } else {
-        // Lexeme already exists, handle as needed
-        printf("Lexeme %s already exists in the symbol table.\n", lexeme);
-    }
-}
-
-// Function to lookup a lexeme in SymbolTable
-bool lookup(char *lexeme) {
-    int index = hash(lexeme);
-
-    // Traverse the linked list at index to find the lexeme
-    SymbolItem *current = table->data[index];
-    while (current != NULL) {
-        if (strcmp(current->lexeme, lexeme) == 0) {
-            // Lexeme found in the symbol table
-            return true;
-        }
-        current = current->next;
-    }
-    // Lexeme not found in the symbol table
-    return false;
-}
+FILE *fp;
+char *forwardPtr, *beginPtr;
+bool firstFlag, secondFlag;
+int lineCount;
+bool isEnd;
+bool firstBufLoadable = true;
+bool secondBufLoadable = true;
+twinBuff twinBuffer;
