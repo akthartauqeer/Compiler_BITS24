d07790c30e8ff355ba117d6901bc0ecf2bba93c4
Unknowncoder09 <111437570+Unknowncoder09@users.noreply.github.com>
2024-03-05 17:04:11 +0530

    Update parser.c

--- parser.c
+++ parser.c
@@ -17,167 +17,312 @@ gitems *createTerminal(terminals t)
     return item;
 }
 
-void populateitems()
-{
-    itemList = (gitems **)malloc((TC + NTC) * sizeof(gitems *));
-    itemList[0] = createTerminal(TK_GT);
-    itemList[1] = createTerminal(TK_LT);
-    itemList[2] = createTerminal(TK_ASSIGNOP);
-    itemList[3] = createTerminal(TK_COMMENT);
-    itemList[4] = createTerminal(TK_EQ);
-    itemList[5] = createTerminal(TK_PLUS);
-    itemList[6] = createTerminal(TK_NE);
-    itemList[7] = createTerminal(TK_LE);
-    itemList[8] = createTerminal(TK_GE);
-    itemList[9] = createTerminal(TK_SQR);
-    itemList[10] = createTerminal(TK_SQL);
-    itemList[11] = createTerminal(TK_OR);
-    itemList[12] = createTerminal(TK_NOT);
-    itemList[13] = createTerminal(TK_AND);
-    itemList[14] = createTerminal(TK_ID);
-    itemList[15] = createTerminal(TK_FIELDID);
-    itemList[16] = createTerminal(TK_DIV);
-    itemList[17] = createTerminal(TK_MUL);
-    itemList[18] = createTerminal(TK_MINUS);
-    itemList[19] = createTerminal(TK_FUNID);
-    itemList[20] = createTerminal(TK_DOT);
-    itemList[21] = createTerminal(TK_CL);
-    itemList[22] = createTerminal(TK_OP);
-    itemList[23] = createTerminal(TK_COLON);
-    itemList[24] = createTerminal(TK_SEM);
-    itemList[25] = createTerminal(TK_RUID);
-    itemList[26] = createTerminal(TK_COMMA);
-    itemList[27] = createTerminal(TK_NUM);
-    itemList[28] = createTerminal(TK_RNUM);
-    itemList[29] = createTerminal(TK_MAIN);
-    itemList[30] = createTerminal(TK_AS);
-    itemList[31] = createTerminal(TK_CALL);
-    itemList[32] = createTerminal(TK_DEFINETYPE);
-    itemList[33] = createTerminal(TK_ELSE);
-    itemList[34] = createTerminal(TK_END);
-    itemList[35] = createTerminal(TK_ENDIF);
-    itemList[36] = createTerminal(TK_ENDWHILE);
-    itemList[37] = createTerminal(TK_ENDRECORD);
-    itemList[38] = createTerminal(TK_ENDUNION);
-    itemList[39] = createTerminal(TK_GLOBAL);
-    itemList[40] = createTerminal(TK_IF);
-    itemList[41] = createTerminal(TK_INPUT);
-    itemList[42] = createTerminal(TK_OUTPUT);
-    itemList[43] = createTerminal(TK_INT);
-    itemList[44] = createTerminal(TK_REAL);
-    itemList[45] = createTerminal(TK_LIST);
-    itemList[46] = createTerminal(TK_PARAMETERS);
-    itemList[47] = createTerminal(TK_PARAMETER);
-    itemList[48] = createTerminal(TK_READ);
-    itemList[49] = createTerminal(TK_WRITE);
-    itemList[50] = createTerminal(TK_RECORD);
-    itemList[51] = createTerminal(TK_UNION);
-    itemList[52] = createTerminal(TK_RETURN);
-    itemList[53] = createTerminal(TK_THEN);
-    itemList[54] = createTerminal(TK_TYPE);
-    itemList[55] = createTerminal(TK_WHILE);
-    itemList[56] = createTerminal(TK_WITH);
-    itemList[57] = createNonTerminal(EPS);
-    itemList[58] = createNonTerminal(END_OF_INPUT);
-    itemList[59] = createNonTerminal(program);
-    itemList[60] = createNonTerminal(mainFunction);
-    itemList[61] = createNonTerminal(otherFunctions);
-    itemList[62] = createNonTerminal(function);
-    itemList[63] = createNonTerminal(input_par);
-    itemList[64] = createNonTerminal(output_par);
-    itemList[65] = createNonTerminal(parameter_list);
-    itemList[66] = createNonTerminal(dataType);
-    itemList[67] = createNonTerminal(primitiveDatatype);
-    itemList[68] = createNonTerminal(constructedDatatype);
-    itemList[69] = createNonTerminal(A);
-    itemList[70] = createNonTerminal(stmts);
-    itemList[71] = createNonTerminal(remaining_list);
-    itemList[72] = createNonTerminal(typeDefinitions);
-    itemList[73] = createNonTerminal(typeDefinition);
-    itemList[74] = createNonTerminal(fieldDefinitions);
-    itemList[75] = createNonTerminal(fieldDefinition);
-    itemList[76] = createNonTerminal(fieldtype);
-    itemList[77] = createNonTerminal(moreFields);
-    itemList[78] = createNonTerminal(declarations);
-    itemList[79] = createNonTerminal(declaration);
-    itemList[80] = createNonTerminal(global_or_not);
-    itemList[81] = createNonTerminal(otherStmts);
-    itemList[82] = createNonTerminal(stmt);
-    itemList[83] = createNonTerminal(assignmentStmt);
-    itemList[84] = createNonTerminal(singleOrRecId);
-    itemList[85] = createNonTerminal(option_single_constructed);
-    itemList[86] = createNonTerminal(oneExpansion);
-    itemList[87] = createNonTerminal(moreExpansions);
-    itemList[88] = createNonTerminal(funCallStmt);
-    itemList[89] = createNonTerminal(outputParameters);
-    itemList[90] = createNonTerminal(inputParameters);
-    itemList[91] = createNonTerminal(iterativeStmt);
-    itemList[92] = createNonTerminal(conditionalStmt);
-    itemList[93] = createNonTerminal(elsePart);
-    itemList[94] = createNonTerminal(ioStmt);
-    itemList[95] = createNonTerminal(arithmeticExpression);
-    itemList[96] = createNonTerminal(expPrime);
-    itemList[97] = createNonTerminal(term);
-    itemList[98] = createNonTerminal(termPrime);
-    itemList[99] = createNonTerminal(factor);
-    itemList[100] = createNonTerminal(highPrecedenceOperators);
-    itemList[101] = createNonTerminal(lowPrecedenceOperators);
-    itemList[102] = createNonTerminal(booleanExpression);
-    itemList[103] = createNonTerminal(var);
-    itemList[104] = createNonTerminal(logicalOp);
-    itemList[105] = createNonTerminal(relationalOp);
-    itemList[106] = createNonTerminal(returnStmt);
-    itemList[107] = createNonTerminal(optionalReturn);
-    itemList[108] = createNonTerminal(idList);
-    itemList[109] = createNonTerminal(more_ids);
-    itemList[110] = createNonTerminal(definetypestmt);
-}
-
-void addRule(int index, non_terminals nt, int size, gitems *value)
-{
+void addrule(int index, non_terminals nt, int size,gitems value[]) {
     LHSNode *lhsNode;
-    if (G->rules[index - 1] == NULL)
-    {
-        lhsNode = (LHSNode *)malloc(sizeof(LHSNode));
-        lhsNode->rules = NULL;
-        lhsNode->lhs = nt;
+    if(G->rules[index-1]==NULL){
+    lhsNode = (LHSNode*)malloc(sizeof(LHSNode));
+    lhsNode->rules=NULL;
+    lhsNode->lhs = nt;
     }
-    ProductionRule *currentrulehead = lhsNode->rules;
-    ProductionRule *newRule = (ProductionRule *)malloc(sizeof(ProductionRule));
+    ProductionRule *currentrulehead=lhsNode->rules;
+    ProductionRule *newRule = (ProductionRule*)malloc(sizeof(ProductionRule));
     newRule->head = NULL;
     newRule->next_rule = NULL;
-    if (currentrulehead == NULL)
-    {
-        lhsNode->rules = newRule;
+    RHSNode* rhshead=(RHSNode *)malloc(sizeof(RHSNode));
+    RHSNode*rhsptr=rhshead;
+    if(currentrulehead!=NULL){
+        while(currentrulehead->next_rule!=NULL)
+        currentrulehead=currentrulehead->next_rule;
     }
-    else
-    {
-        while (currentrulehead->next_rule != NULL)
-            currentrulehead = currentrulehead->next_rule;
-    }
-    // Create and add RHS nodes for the production rule
-    RHSNode *rhshead = (RHSNode *)malloc(sizeof(RHSNode));
-    RHSNode *rhsptr = rhshead;
-    rhshead->v = value[0].v;
-    rhshead->isT = value[0].isTer;
-    for (int i = 1; i < size; i++)
-    {
-        // Create a new RHSNode
+    rhshead->v=value[0].v;
+    rhshead->isT=value[0].isTer;
+    for(int i = 1; i < size; i++) {
         RHSNode *rhsNode = (RHSNode *)malloc(sizeof(RHSNode));
-        rhshead->v = value[i].v;
-        rhshead->isT = value[i].isTer;
+        rhsNode->v=value[i].v;
+        rhsNode->isT=value[i].isTer;
+        rhsNode->next=NULL;
         rhsptr->next = rhsNode;
-        rhsNode->ptr = NULL; // You might need to set this depending on your needs
-        // Add the RHSNode to the production rule
-        rhsptr = rhsptr->next;
+        rhsNode->ptr = NULL;
+        rhsptr=rhsptr->next;
     }
-
-    // Link the new production rule to the LHSNode
-    newRule->head = rhshead;
-    currentrulehead->next_rule = newRule;
+    newRule->head=rhshead;
+    if(currentrulehead==NULL){
+        currentrulehead=newRule;
+    }
+    else{
+    currentrulehead->next_rule=newRule;
+    }
     // Add the LHSNode to the grammar
-    G->rules[index] = lhsNode;
+    G->rules[index-1] = lhsNode;
 }
+    void addGrammarRules()
+{   
+    gitems v1[2]={{false,otherFunctions},{false,mainFunction}};
+    addrule(1, program, 2, v1);
+    gitems v2[3]={{true,TK_MAIN},{false,stmts},{true,TK_END}};
+    addrule(2, mainFunction, 3, v2);
+    gitems v3[2]={{false,function},{false,otherFunctions}};
+    addrule(3, otherFunctions, 2, v3);
+    gitems v4[1] = {{true, EPS}};
+    addrule(3, otherFunctions, 1, v4);
+
+    gitems v5[6] = {{ true,TK_FUNID}, {false, input_par}, {false, output_par}, {true, TK_SEM}, {false, stmts}, {true, TK_END}};
+    addrule(4, function, 6, v5);
+
+    gitems v6[6] = {{true, TK_INPUT}, {true, TK_PARAMETER}, {true, TK_LIST}, {true, TK_SQL}, {false, parameter_list}, {true, TK_SQR}};
+    addrule(5, input_par, 6, v6);
+
+    gitems v7[6] = {{true, TK_OUTPUT}, {true, TK_PARAMETER},{true, TK_LIST}, {true, TK_SQL},{false, parameter_list}, {true, TK_SQR}};
+    addrule(6, output_par, 6, v7);
+
+    gitems v8[1] = {{true, EPS}};
+    addrule(6, output_par, 1, v8);
+
+    gitems v9[3] = {{false, dataType}, {true, TK_ID}, {false, remaining_list}};
+    addrule(7, parameter_list, 3, v9);
+
+    gitems v10[1] = {{false, primitiveDatatype}};
+    addrule(8, dataType,1, v10);
+
+    gitems v11[1] = {{false, constructedDatatype}};
+    addrule(8, dataType, 1, v11);
+
+    gitems v12[1] = {{true, TK_INT}};
+    addrule(9, primitiveDatatype, 1, v12);
+
+    gitems v13[1] = {{true, TK_REAL}};
+    addrule(9, primitiveDatatype, 1, v13);
+
+    gitems v14[2] = {{true, TK_RECORD}, {true, TK_RUID}};
+    addrule(10, constructedDatatype, 2, v14);
+
+    gitems v15[2] = {{true, TK_UNION}, {true, TK_RUID}};
+    addrule(10, constructedDatatype, 2, v15);
+
+    gitems v16[1] = {{true, TK_RUID}};
+    addrule(10, constructedDatatype, 1, v16);
+
+    gitems v19[4] = {{false, typeDefinitions}, {false,declarations}, {false,otherStmts}, {false,returnStmt}};
+    addrule(12,stmts,4,v19);
+
+    gitems v17[2] = {{true, TK_COMMA}, {false, parameter_list}};
+    addrule(13, remaining_list, 2, v17);
+
+    
+    gitems v18[1] = {{true, EPS}};
+    addrule( 13, remaining_list, 1 , v18);
+
+    gitems v20[2] = {{false,typeDefinition}, {false, typeDefinitions}};
+    addrule(14,typeDefinitions,2,v20);
+
+    gitems v21[1] = {{true,EPS}};
+    addrule(14,typeDefinitions, 1, v21);
+    
+    gitems v22[4] = {{true, TK_RECORD}, {true,TK_RUID}, {false, fieldDefinitions}, {true, TK_ENDRECORD}};
+    addrule(15,typeDefinition,4,v22);
+
+    gitems v23[4] = {{true,TK_UNION}, {true, TK_RUID}, {false, fieldDefinitions}, {true, TK_ENDUNION}};
+    addrule(15,typeDefinition,4,v23);
+
+    gitems v24[3] = {{false,fieldDefinition}, {false,fieldDefinition}, {false,moreFields}};
+    addrule(16, fieldDefinitions, 3, v24);
+
+    gitems v25[5] = {{true, TK_TYPE}, {false,fieldtype}, {true,TK_COLON}, {true,TK_FIELDID}, {true, TK_SEM}};
+    addrule(17, fieldDefinition, 5, v25);
+
+    gitems v26[1] = {{false,primitiveDatatype}};
+    addrule(18,fieldtype,1,v26);
+
+    gitems v27[1] = {{true, TK_RUID}};
+    addrule(18,fieldtype,1,v27);
+
+    gitems v28[2] = {{false,fieldDefinition}, {false, moreFields}};
+    addrule(19,moreFields,2, v28);
+
+    gitems v29[1] = {{true,EPS}};
+    addrule(19,moreFields,1,v29);
+
+    gitems v30[2] = {{false,declaration}, {false, declarations}};
+    addrule(20,declarations,2,v30);
+
+    gitems v31[1] = {{true, EPS}};
+    addrule(20,declarations,1,v31);
+
+    gitems v32[6] = {{true,TK_TYPE}, {false, dataType}, {true,TK_COLON},{true,TK_ID}, {false, global_or_not},{true, TK_SEM}};
+    addrule(21, declaration, 6, v32);
+
+    gitems v33[2] = {{true,TK_COLON}, {true, TK_GLOBAL}};
+    addrule(22, global_or_not, 2, v33);
+
+    gitems v34[1] = {{true, EPS}};
+    addrule(22,global_or_not,1,v34);
+
+    gitems v35[2] = {{false, stmt}, {false, otherStmts}};
+    addrule(23,otherStmts,2,v35);
+
+    gitems v36[1] = {{true, EPS}};
+    addrule(23,otherStmts,1,v36);
+
+    gitems v37[1] = {{false, assignmentStmt}};
+    addrule(24,stmt, 1, v37);
+
+    gitems v38[1] = {{false, iterativeStmt}};
+    addrule(24, stmt, 1, v38);
+
+    gitems v39[1] = {{false, conditionalStmt}};
+    addrule(24,stmt,1,v39);
+
+    gitems v40[1] = {{false, ioStmt}};
+    addrule(24,stmt,1,v40);
+
+    gitems v41[1] = {{false, funCallStmt}};
+    addrule(24,stmt,1,v41);
+
+    gitems v42[4] = {{false, singleOrRecId}, {true, TK_ASSIGNOP}, {false, arithmeticExpression}, {true, TK_SEM}};
+    addrule(25,assignmentStmt, 4, v42);
+
+    gitems v43[2] = {{true, TK_ID}, {false,option_single_constructed}};
+    addrule(26,singleOrRecId, 2, v43);
+
+    gitems v44[2] = {{true,TK_DOT}, {false,option_single_constructed}};
+    addrule(28,oneExpansion,2,v44);
+
+    gitems v45[2] = {{false,oneExpansion}, {false,moreExpansions}};
+    addrule(29,moreExpansions,2,v45);
+
+    gitems v46[1] = {{true,EPS}};
+    addrule(29,moreExpansions,1,v46);
+
+    gitems v47[7] ={{true,TK_SQL}, {false,idList},{true, TK_SQR}, {true,TK_ASSIGNOP},{true,EPS}};
+    addrule(31, outputParameters, 4,v47);
+
+    gitems v48[3] = {{true, TK_SQL}, {false, idList}, {true, TK_SQR}};
+    addrule(32, inputParameters, 3, v48);
+
+    gitems v49[7] = {{true,TK_WHILE}, {true, TK_OP}, {false,booleanExpression}, {true, TK_CL}, {false, stmt}, {false,otherStmts}, {true, TK_ENDWHILE}};
+    addrule(33,iterativeStmt, 7, v49);
+
+    gitems v50[8] = {{true, TK_IF}, {false, TK_OP}, {false, booleanExpression}, {true, TK_CL}, {true, TK_THEN}, {false, stmt}, {false, otherStmts}, {false,elsePart}};
+    addrule(34,conditionalStmt, 8, v50);
+
+    gitems v51[4] = {{true, TK_ELSE}, {false, stmt}, {false, otherStmts}, {true, TK_ENDIF}};
+    addrule(35,elsePart,4 ,v51);
+
+    gitems v52[1] = {{true, TK_ENDIF}};
+    addrule(35,elsePart, 1 ,v52);
+
+    gitems v53[5] = {{true, TK_WRITE}, {true,TK_OP}, {false,var}, {true,TK_CL}, {true, TK_SEM}};
+    addrule(36, ioStmt, 5, v53);
+
+    gitems v54[5] = {{true, TK_WRITE}, {true,TK_OP}, {false,var}, {true, TK_CL}, {true, TK_SEM}};
+    addrule(36,ioStmt,5,v54);
+
+    gitems v55[2] ={{false, term}, {false, expPrime}};
+    addrule(37, arithmeticExpression, 2, v55);
+
+    gitems v56[3] ={{false, lowPrecedenceOperators}, {false, term}, {false, expPrime}};
+    addrule(38, expPrime, 3, v56);
+
+    gitems v57[1] ={{true, EPS}};
+    addrule(38, expPrime, 1, v57);
+
+    gitems v58[2] ={{false, factor}, {false, termPrime}};
+    addrule(39, term, 2, v58);
+
+    gitems v59[3] ={{false, highPrecedenceOperators}, {false, factor}, {false, termPrime}};
+    addrule(40, termPrime, 3, v59);
+
+    gitems v60[1] ={{true, EPS}};
+    addrule(40, termPrime, 2, v60);
+
+    gitems v61[3] ={{true, TK_OP}, {false, arithmeticExpression}, {true,TK_CL}};
+    addrule(41, factor, 3, v61);
+
+    gitems v62[1] ={{false, var}};
+    addrule(41, factor, 1, v62);
+
+    gitems v63[1] ={{true, TK_MUL}};
+    addrule(42, highPrecedenceOperators, 1, v63);
+
+    gitems v64[1] ={{true, TK_DIV}};
+    addrule(42, highPrecedenceOperators, 1, v64);
+
+    gitems v65[1] ={{true, TK_PLUS}};
+    addrule(43, lowPrecedenceOperators, 2, v65);
+
+    gitems v66[1] ={{true, TK_MINUS}};
+    addrule(43, lowPrecedenceOperators, 1, v66);
+
+    gitems v67[7] ={{true, TK_OP}, {false, booleanExpression}, {true, TK_CL}, {true, TK_AND}, {true, TK_OP}, {false, booleanExpression}, {true, TK_CL}};
+    addrule(44, booleanExpression, 7, v67);
+
+    gitems v68[3] ={{false, var}, {true, relationalOp}, {false, var}};
+    addrule(44, booleanExpression, 3, v68);
+
+    gitems v69[4] ={{true, TK_NOT}, {true, TK_OP}, {false, booleanExpression}, {true, TK_CL}};
+    addrule(44, booleanExpression, 4, v69);
+
+    gitems v70[1] = {{false, singleOrRecId}};
+    addrule(45, var, 1, v70);
+
+    gitems v71[1] = {{true, TK_NUM}};
+    addrule(47,var,1,v71);
+
+    gitems v72[1] = {{true, TK_RNUM}};
+    addrule(45,var,1,v72);
+
+    gitems v73[1] = {{true, TK_AND}};
+    addrule(46,logicalOp, 1, v73);
+
+    gitems v74[1] = {{true, TK_OR}};
+    addrule(46,logicalOp, 1 ,v74);
+
+    gitems v75[1] = {{true, TK_LT}};
+    addrule(47,relationalOp,1,v75);
+
+    gitems v76[1] = {{true, TK_LE}};
+    addrule(47,relationalOp, 1, v76);
+
+    gitems v77[1] = {{true, TK_EQ}};
+    addrule(47,relationalOp, 1, v77);
+    
+    gitems v78[1] = {{true, TK_GT}};
+    addrule(47,relationalOp, 1, v78);
+
+    gitems v79[1] = {{true, TK_GE}};
+    addrule(47, relationalOp, 1, v79);
+
+    gitems v80[1] = {{true, TK_NE}};
+    addrule(47,relationalOp, 1, v80);
+
+    gitems v81[3] = {{true, TK_RETURN}, {false, optionalReturn}, {true, TK_SEM}};
+    addrule(48, returnStmt, 3, v81);
+
+    gitems v82[4] = {{true, TK_SQL}, {false, idList}, {true, TK_SQR}, {true, TK_ASSIGNOP}};
+    addrule(49, optionalReturn, 4, v82);
+
+    gitems v83[1] = {{true, EPS}};
+    addrule(49, optionalReturn, 1, v83);
+
+    gitems v84[2] = {{true, TK_ID}, {false,more_ids}};
+    addrule(50,idList, 2, v84);
+
+    gitems v85[2] = {{true, TK_COMMA}, {false, idList}};
+    addrule(51, more_ids, 2, v85);
+
+    gitems  v86[1] = {{true, EPS}};
+    addrule(51, more_ids, 1, v86);
+
+    gitems v87[5] = {{true, TK_DEFINETYPE}, {false, A}, {true, TK_AS}, {true, TK_RUID}};
+    addrule(52,definetypestmt, 5, v87);
+
+    gitems v88[1] = {{true, TK_RECORD}};
+    addrule(11, A, 1, v88);
+
+    gitems v89[1] = {{true, TK_UNION}};
+    addrule(11,A,1,v89);
+}
+
 
 void addGrammarRules()
 {
