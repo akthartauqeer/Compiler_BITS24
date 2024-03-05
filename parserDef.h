6e5cd3b27abe91d4fab1b99c9906efe6598d80ae
Dhruv Kamra <95697310+Dhruvi-2003@users.noreply.github.com>
2024-03-05 19:35:04 +0530

    Update parserDef.h

--- parserDef.h
+++ parserDef.h
@@ -1,86 +1,109 @@
 #ifndef parserDef
 #define parserDef
-#include"lexerDef.h"
+#include "lexerDef.h"
 
 #endif
 
-//First follow sets
-typedef struct terminal_node {
+// First follow sets
+typedef struct terminal_node
+{
     terminals t;
-    struct terminal_node* next;
+    struct terminal_node *next;
 } terminal_node;
 
-typedef struct terminal_list {
-    terminal_node* head;
+typedef struct terminal_list
+{
+    terminal_node *head;
 } terminal_list;
 
-typedef struct non_terminal_sets {
-    terminal_list* first_set;
-    terminal_list* follow_set;
+typedef struct non_terminal_sets
+{
+    terminal_list *first_set;
+    terminal_list *follow_set;
 } non_terminal_sets;
 
-//Nodes of Grammar Non-Terminals
-typedef union u {
-        terminals t;            // Terminal value
-        non_terminals non_t;     // Non-terminal value
-    } value;
-typedef struct rhs_node {
-    bool isT;  // Indicates whether it's a terminal or non-terminal
+// Nodes of Grammar Non-Terminals
+typedef union u
+{
+    terminals t;         // Terminal value
+    non_terminals non_t; // Non-terminal value
+} value;
+
+typedef struct rhs_node
+{
+    bool isT; // Indicates whether it's a terminal or non-terminal
     value v;
-    struct rhs_node* next;  // Pointer to the next node in the list
-    treeNode* ptr;
+    struct rhs_node *next; // Pointer to the next node in the list
+    treeNode *ptr;
 } RHSNode;
 
 // A linked list containing a production rule
-typedef struct production_rule {
-    RHSNode* head;          // Pointer to the head of the list
-    struct production_rule* next_rule;  // Pointer to the next production rule
+typedef struct production_rule
+{
+    RHSNode *head;                     // Pointer to the head of the list
+    struct production_rule *next_rule; // Pointer to the next production rule
 } ProductionRule;
 
 // Node for RHS of production rule
-typedef struct lhs_node {
-    ProductionRule* rules;  // Pointer to linked list of production rules
-    non_terminals lhs;       // LHS non-terminal value
+typedef struct lhs_node
+{
+    ProductionRule *rules; // Pointer to linked list of production rules
+    non_terminals lhs;     // LHS non-terminal value
 } LHSNode;
 
-typedef struct grammar {
-    LHSNode* rules[NTC];
-} Grammar; 
+typedef struct grammar
+{
+    LHSNode *rules[NTC];
+} Grammar;
 
-typedef struct parse_table {
-    ProductionRule* table[NTC][TC];
+typedef struct parse_table
+{
+    ProductionRule *table[NTC][TC];
 } ParseTable;
 
-//parse tree
-typedef struct tree_node {
-    treeNode* parent;  
-    treeNode* firstChild;   
-    treeNode* rightSibling;   
-    token* token_ptr;          // Pointer to the token associated with the node
-    bool isT;                  
+// parse tree
+typedef struct tree_node
+{
+    treeNode *parent;
+    treeNode *firstChild;
+    treeNode *rightSibling;
+    SymbolItem *token_ptr; // Pointer to the token associated with the node
+    bool isT;
     value v;
 } treeNode;
 
-//stack for maintaining non-terminals
-typedef struct stack {
-    LHSNode* top;
+// stack for maintaining non-terminals
+typedef struct stack
+{
+    LHSNode *top;
     int size;
 } helperStack;
 
-//follow for non-terminals initialized
-typedef struct follow {
-    RHSNode *occurrence;  
+// follow for non-terminals initialized
+typedef struct follow
+{
+    RHSNode *occurrence;
     non_terminals parent_nt;
-    struct follow *next;  // Pointer to the next follow struct
+    struct follow *next; // Pointer to the next follow struct
 } followDS;
-typedef struct {
+
+typedef struct
+{
     bool isTer; // Flag indicating if it's a terminal or not
     value v;
 } gitems;
 
-followDS *follow_occurrence[NTC]; 
+typedef struct StackNode
+{
+    bool isT;
+    value value;
+    struct StackNode *next;
+}StackNode;
+
+followDS *follow_occurrence[NTC];
 Grammar *G;
-non_terminal_sets first_follow_sets[NTC]; 
-ParseTable* PT;
-helperStack* Stack;
-treeNode* root; 
+non_terminal_sets first_follow_sets[NTC];
+ParseTable *PT;
+helperStack *Stack;
+treeNode *root;
+gitems **itemList;
