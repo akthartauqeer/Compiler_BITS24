d4ee91dfc0ff1868cca6ac1ab7eea93252b438f3
salonibhandari13 <83011525+salonibhandari13@users.noreply.github.com>
2024-03-05 16:54:01 +0530

    final lexer.h

--- lexer.h
+++ lexer.h
@@ -1,12 +1,21 @@
 #include"lexerDef.h"
-// Function prototypes
+
+/*
+GROUP_NO_39
+Dhruv Kamra - 2021A7PS0713P
+Navneet Singla - 2021A7PS1450P
+Tauqeer Akthar- 2021A7PS1628P
+Archisha Mehta - 2020B3A70779P
+Saloni Bhandari - 2020B1A71602P 
+*/
+
 FILE *initialise(char *inputFile, long long int buff_size);
 void initializeSymbolTable();
 int hash(char *lexeme);
 void insert(char *lexeme, terminals token);
 bool lookup(char *lexeme);
 void initializeKeywords();
-SymbolItem errorHandle(int err, char *lex, int line);
+SymbolItem error_helper(int error, char *lex, int line);
 char *getLexeme();
 FILE *getstream(FILE *fp);
 void removeComments(char* testFile, char* cleanFile);
@@ -14,3 +23,7 @@ char getNextCharacter(FILE *fp);
 terminals findKeyword(char *lexeme);
 SymbolItem tokenize(char *lex, terminals g, int line);
 SymbolItem getToken(FILE *fp);
+TokenNode* createTokenNode(terminals token, char* lexeme, int lineNo);
+void appendTokenNode(TokenNode** head, terminals token, char* lexeme, int lineNo);
+void printTokens(TokenNode* head);
+void freeTokenList(TokenNode* head);
