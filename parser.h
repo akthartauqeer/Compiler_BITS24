bcfbb35d2aadb11bc5b73cb02ad3007649ab3329
Dhruv Kamra <95697310+Dhruvi-2003@users.noreply.github.com>
2024-03-05 17:33:24 +0530

    Update parser.h

--- parser.h
+++ parser.h
@@ -1,1 +1,24 @@
 #include "parserDef.h"
+gitems* createNonTerminal(non_terminals nt);
+gitems* createTerminal(terminals t);
+void populateitems();
+void addRule();
+void addGrammarRules();
+void create_parse_table();
+void initiate_parse_table();
+void print_parse_table();
+terminal_list * compute_first_for_rhsnode(RHSNode* rhs , terminal_list* firstset);
+void join_terminallist_exc_eps(terminal_list* list1, terminal_list* list2);
+void add_terminal_tolist(terminal_list *list , terminals t);
+void join_terminal_list(terminal_list * l1 , terminal_list* l2);
+void initialise_list(terminal_list* list);
+bool contains_epsilon(terminal_list * given_list);
+void remove_epsilon(terminal_list *given_list);
+void compute_first(non_terminals given_nt);
+void compute_firsts();
+void populate_occ_follow();
+void find_followset(non_terminals nt);
+void join_terminallist_exc_eps(terminal_list* list1, terminal_list* list2);
+void compute_follow();
+bool createParseTree(SymbolItem *head);
+void printParseTree(treeNode* node, int depth);
